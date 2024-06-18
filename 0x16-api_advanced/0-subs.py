82c44500292c4fed36e2365bd41c6b22eba29e43
yahya <cherifyahya069@gmail.com>
2024-06-18 10:08:52 -0700

    api advanced

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,20 +1,18 @@
 #!/usr/bin/python3
-"""
-Queries the Reddit API and returns the number of total subscribers for a given
-subreddit.
-"""
+"""Module for task 0."""
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """
-    Queries the Reddit API and returns the number of total subscribers for a
-    given subreddit.
-    """
-    url = 'http://www.reddit.com/r/{}/about.json'.format(subreddit)
-    headers = {'User-Agent': 'Python/1.0(alx 0x16 task 0)'}
-    response = requests.get(url, headers=headers)
-    if (not response.ok):
+    """Return the number of subscribers on a given subreddit."""
+    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
+    headers = {
+        "User-Agent": "My-User-Agent"
+    }
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code == 404:
         return 0
-    subscriber_count = response.json().get('data').get('subscribers')
-    return subscriber_count
+    else:
+        results = response.json()
+        subscribers = results.get('data')['subscribers']
+        return subscribers
