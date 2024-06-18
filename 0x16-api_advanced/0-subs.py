d3d07523c4b4c6667dca8758077dfc6527c51541
yahya <cherifyahya069@gmail.com>
2024-06-18 10:46:46 -0700

    api advanced

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,17 +1,18 @@
 #!/usr/bin/python3
-"""Function to print hot posts on a given Reddit subreddit."""
+"""Module for task 0."""
 import requests
 
 
-def top_ten(subreddit):
-    """Print the titles of the 10 hottest posts on a given subreddit."""
-    url = "https://www.reddit.com/r/{}/hot/.json?limit=10".format(subreddit)
+def number_of_subscribers(subreddit):
+    """Return the number of subscribers on a given subreddit."""
+    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
     headers = {
-        'User-Agent': 'alxapp/1.0 (by /u/SimoPablo)'
+        "User-Agent": "My-User-Agent"
     }
     response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code == 404:
-        print("None")
-        return
-    results = response.json().get("data")
-    [print(c.get("data").get("title")) for c in results.get("children")]
+        return 0
+    else:
+        results = response.json()
+        subscribers = results.get('data')['subscribers']
+        return subscribers
